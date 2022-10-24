7e9fd4145cdd22047516035edc97394908903487
drewzambelli <drew.coding.freelance@gmail.com>
2022-10-24 17:49:03 -0400

    prefix fl

---
+++ prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string result;
+        for (int i = 0; i<strs[0].size(); i++) {
+            for (int j = 1; j<strs.size(); j++) {
+                if(strs[0][i] != strs[j][i]) {
+                    return result;
+                }
+            } 
+            result += strs[0][i];    
+        }
+     return result;   
+    }
+    
+};
